4f7bba4510402446b2188ddbe0b170ad6634e8fc
leonel ka <waboleonel@gmail.com>
2024-09-30 00:19:14 +0100

    Testing customer service the light way - continue

---
+++ services/customer-service/src/test/java/com/workbeatstalent/customerservice/customer/CustomerServiceWithMockitoTest.java
@@ -0,0 +1,80 @@
+package com.workbeatstalent.customerservice.customer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerServiceWithMockitoTest {
+
+    @Test
+    void create() {
+        // GIVEN
+
+        // WHEN
+
+        // THEN
+    }
+
+    @Test
+    void update() {
+        // GIVEN
+
+        // WHEN
+
+        // THEN
+    }
+
+    @Test
+    void getById() {
+        // GIVEN
+
+        // WHEN
+
+        // THEN
+    }
+
+    @Test
+    void getAll() {
+        // GIVEN
+
+        // WHEN
+
+        // THEN
+    }
+
+    @Test
+    void deleteById() {
+        // GIVEN
+
+        // WHEN
+
+        // THEN
+    }
+
+    @Test
+    void existsById() {
+        // GIVEN
+
+        // WHEN
+
+        // THEN
+    }
+
+    @Test
+    void existsByFirstname() {
+        // GIVEN
+
+        // WHEN
+
+        // THEN
+    }
+
+    @Test
+    void existsByLastname() {
+        // GIVEN
+
+        // WHEN
+
+        // THEN
+    }
+}
