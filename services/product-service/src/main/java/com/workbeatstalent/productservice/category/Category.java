520439b0719da291cd64b2171d89d776681b0d7a
leonel ka <waboleonel@gmail.com>
2024-09-30 23:12:43 +0100

    Product service - update models

--- services/product-service/src/main/java/com/workbeatstalent/productservice/category/Category.java
+++ services/product-service/src/main/java/com/workbeatstalent/productservice/category/Category.java
@@ -18,7 +18,7 @@ import java.util.List;
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
     private String name;
     private String description;
     @OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE) // REMOVE - if a category is removed, this will also removed related products
