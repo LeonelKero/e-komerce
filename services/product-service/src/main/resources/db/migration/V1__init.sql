95a04f13c4920a608c4cb9ca17b6f262579ff9fe
leonel ka <waboleonel@gmail.com>
2024-09-30 23:13:01 +0100

    Product service - flyway initial migration

---
+++ services/product-service/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,18 @@
+create table if not exists category(
+    id integer primary key,
+    name varchar(255) not null,
+    description varchar(255)
+);
+
+create table if not exists product(
+    id integer primary key,
+    name varchar(255) not null,
+    description varchar(255),
+    available_quantity integer not null check(available_quantity > -1),
+    price numeric(32, 2) not null check(price > 0),
+    category_id integer constraint fk_category_id references category
+);
+
+create sequence if not exists category_sequence increment by 1;
+
+create sequence if not exists product_sequence increment by 1;
