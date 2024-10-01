21f71d059f7729be5c0bfc17d3ae9b0e571cd78d
leonel ka <waboleonel@gmail.com>
2024-10-01 16:45:33 +0100

    Product service - Definition of repositories

---
+++ services/product-service/src/main/java/com/workbeatstalent/productservice/product/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.workbeatstalent.productservice.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    Optional<Product> findByName(final String name);
+
+}
