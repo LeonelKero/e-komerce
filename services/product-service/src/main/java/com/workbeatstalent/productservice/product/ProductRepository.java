db2f779a6f99473d1b6fc5e5dde2efbea4f5fffc
leonel ka <waboleonel@gmail.com>
2024-10-02 19:49:14 +0100

    Product service - implement the repository

--- services/product-service/src/main/java/com/workbeatstalent/productservice/product/ProductRepository.java
+++ services/product-service/src/main/java/com/workbeatstalent/productservice/product/ProductRepository.java
@@ -2,10 +2,13 @@ package com.workbeatstalent.productservice.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
     Optional<Product> findByName(final String name);
+
+    List<Product> findAllByIdInOrderById(final List<Integer> ids);
 
 }
