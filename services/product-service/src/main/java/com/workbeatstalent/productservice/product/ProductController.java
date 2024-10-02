8425461e3f09b7a1bc6f765f558b32f463055b6e
leonel ka <waboleonel@gmail.com>
2024-10-02 19:50:02 +0100

    Product service - implement the controller layer

---
+++ services/product-service/src/main/java/com/workbeatstalent/productservice/product/ProductController.java
@@ -0,0 +1,43 @@
+package com.workbeatstalent.productservice.product;
+
+import com.workbeatstalent.productservice.product.dto.ProductPurchaseRequest;
+import com.workbeatstalent.productservice.product.dto.ProductPurchaseResponse;
+import com.workbeatstalent.productservice.product.dto.ProductRequest;
+import com.workbeatstalent.productservice.product.dto.ProductResponse;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = {"/api/v1/products"})
+public class ProductController {
+
+    private final ProductService service;
+
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<Integer> addProduct(final @Valid @RequestBody ProductRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(request));
+    }
+
+    @PostMapping(path = {"/purchases"})
+    public ResponseEntity<List<ProductPurchaseResponse>> allPurchases(final @RequestBody List<ProductPurchaseRequest> purchaseRequest) {
+        return ResponseEntity.ok(this.service.purchases(purchaseRequest));
+    }
+
+    @GetMapping(path = {"/{id}"})
+    public ResponseEntity<ProductResponse> getById(final @PathVariable Integer id) {
+        return ResponseEntity.ok(this.service.getById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> allProducts() {
+        return ResponseEntity.ok(this.service.getAll());
+    }
+}
