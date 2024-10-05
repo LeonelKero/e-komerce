c8a539ca34f21dff1ae73520d7825455a02c5324
leonel ka <waboleonel@gmail.com>
2024-10-05 22:23:02 +0100

    Order service - definition of order entity

---
+++ services/order-service/src/main/java/com/workbeattalent/orderservice/order/Order.java
@@ -0,0 +1,46 @@
+package com.workbeattalent.orderservice.order;
+
+import com.workbeattalent.orderservice.lines.OrderLine;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "customer_orders")
+@EntityListeners({AuditingEntityListener.class})
+public class Order {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String reference;
+    private BigDecimal totalAmount;
+
+    @Enumerated(value = EnumType.STRING)
+    private PaymentMethod paymentMethod;
+
+    private String customerId;
+
+    @OneToMany(mappedBy = "order")
+    private Set<OrderLine> lines = new HashSet<>();
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime lastModifiedAt;
+}
