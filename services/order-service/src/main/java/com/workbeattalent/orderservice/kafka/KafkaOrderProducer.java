29eacd3246b6b6b5ff648873027adb9857a81b9f
leonel ka <waboleonel@gmail.com>
2024-10-09 09:26:18 +0100

    Order service - define order producer

---
+++ services/order-service/src/main/java/com/workbeattalent/orderservice/kafka/KafkaOrderProducer.java
@@ -0,0 +1,27 @@
+package com.workbeattalent.orderservice.kafka;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class KafkaOrderProducer {
+
+    private final KafkaTemplate<String, OrderConfirmation> orderConfirmationKafkaTemplate;
+
+    public void orderConfirmation(final OrderConfirmation confirmation) {
+        log.info("Sending order confirmation...");
+        final var message = MessageBuilder
+                .withPayload(confirmation)
+                .setHeader(KafkaHeaders.TOPIC, "order-topic")
+                .build();
+
+        orderConfirmationKafkaTemplate.send(message);
+    }
+
+}
