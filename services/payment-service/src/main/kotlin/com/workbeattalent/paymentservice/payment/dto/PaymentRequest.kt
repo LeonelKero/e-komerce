9d3e3ff6e3988766849c8146cd0d2afe1dcab83e
leonel ka <waboleonel@gmail.com>
2024-10-10 16:27:37 +0100

    Payment service - add validation constraints

--- services/payment-service/src/main/kotlin/com/workbeattalent/paymentservice/payment/dto/PaymentRequest.kt
+++ services/payment-service/src/main/kotlin/com/workbeattalent/paymentservice/payment/dto/PaymentRequest.kt
@@ -1,13 +1,18 @@
 package com.workbeattalent.paymentservice.payment.dto
 
 import com.workbeattalent.paymentservice.payment.PaymentMethod
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Positive
 import java.math.BigDecimal
 
 data class PaymentRequest(
     val id: Long?,
+    @Positive(message = "Amount requires a positive value")
     val amount: BigDecimal,
     val paymentMethod: PaymentMethod,
+    @NotNull(message = "Order identifier required")
     val orderId: Long,
+    @NotNull(message = "Order reference required")
     val orderRef: String,
     val customer: Customer
 )
