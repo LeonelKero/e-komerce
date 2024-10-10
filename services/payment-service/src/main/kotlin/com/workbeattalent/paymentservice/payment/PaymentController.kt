6d836007f728a5e8b8c569104512ec7e0288c1ec
leonel ka <waboleonel@gmail.com>
2024-10-10 17:05:12 +0100

    Payment service - implement controller layer

---
+++ services/payment-service/src/main/kotlin/com/workbeattalent/paymentservice/payment/PaymentController.kt
@@ -0,0 +1,20 @@
+package com.workbeattalent.paymentservice.payment
+
+import com.workbeattalent.paymentservice.payment.dto.PaymentRequest
+import jakarta.validation.Valid
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping(path = ["/api/v1/payments"])
+class PaymentController(private val service: PaymentService) {
+
+    @PostMapping
+    fun makePayment(@Valid @RequestBody paymentRequest: PaymentRequest): ResponseEntity<Long> {
+        return ResponseEntity.ok(service.createPayment(paymentRequest))
+    }
+
+}
