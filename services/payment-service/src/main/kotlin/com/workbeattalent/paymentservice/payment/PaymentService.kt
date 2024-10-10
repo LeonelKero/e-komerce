4757b01b13ec51275405a51f4eef7058fc23ad4d
leonel ka <waboleonel@gmail.com>
2024-10-10 17:05:27 +0100

    Payment service - implement service layer

---
+++ services/payment-service/src/main/kotlin/com/workbeattalent/paymentservice/payment/PaymentService.kt
@@ -0,0 +1,34 @@
+package com.workbeattalent.paymentservice.payment
+
+import com.workbeattalent.paymentservice.payment.dto.PaymentRequest
+import com.workbeattalent.paymentservice.payment.kafka.NotificationProducer
+import com.workbeattalent.paymentservice.payment.kafka.PaymentNotificationRequest
+import com.workbeattalent.paymentservice.payment.mapper.PaymentMapper
+import org.springframework.stereotype.Service
+
+@Service
+class PaymentService(
+    private val paymentRepository: PaymentRepository,
+    private val mapper: PaymentMapper,
+    private val notificationProducer: NotificationProducer
+) {
+
+    fun createPayment(request: PaymentRequest): Long {
+        // Persist payment
+        val payment = this.paymentRepository.save(mapper.toPayment(request))
+        // Send message to the notification service
+        notificationProducer.sendNotification(
+            PaymentNotificationRequest(
+                request.orderRef,
+                request.amount,
+                request.paymentMethod,
+                request.customer.firstname,
+                request.customer.lastname,
+                request.customer.email
+            )
+        )
+        // Return payment ID
+        return payment.id!!
+    }
+
+}
