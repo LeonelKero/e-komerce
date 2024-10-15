626b417d97656d4d7dcff74bd238cdab5158f21c
leonel ka <waboleonel@gmail.com>
2024-10-15 23:24:55 +0100

    Product service - Update flyway init script

--- services/product-service/src/main/resources/db/migration/V1__init.sql
+++ services/product-service/src/main/resources/db/migration/V1__init.sql
@@ -1,9 +1,11 @@
+create sequence if not exists product_seq increment by 50;
+create sequence if not exists category_seq increment by 50;
+
 create table if not exists category(
     id integer primary key,
     name varchar(255) not null,
     description varchar(255)
 );
---create sequence if not exists category_seq increment by 1;
 
 create table if not exists product(
     id integer primary key,
@@ -13,8 +15,7 @@ create table if not exists product(
     price numeric(32, 2) not null check(price > 0),
     category_id integer constraint fk_category_id references category
 );
---create sequence if not exists product_seq increment by 1;
 
-
---create sequence if not exists category_seq start with 1;
---create sequence if not exists product_seq start with 1;
+-- Ensure sequence ownership
+alter sequence product_seq owned by product.id;
+alter sequence category_seq owned by category.id;
