92eef6a3886e664147be49408444444e00862a49
leonel ka <waboleonel@gmail.com>
2024-10-20 21:34:35 +0100

    Order service - code refactoring to avoid json infinit loop with order

--- services/order-service/src/main/java/com/workbeattalent/orderservice/lines/OrderLine.java
+++ services/order-service/src/main/java/com/workbeattalent/orderservice/lines/OrderLine.java
@@ -1,5 +1,6 @@
 package com.workbeattalent.orderservice.lines;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.workbeattalent.orderservice.order.Order;
 import jakarta.persistence.*;
 import lombok.*;
@@ -16,6 +17,7 @@ public class OrderLine {
     private Long id;
     @ManyToOne
     @JoinColumn(name = "order_id")
+    @JsonBackReference
     private Order order;
     private Integer productId;
     private Integer quantity;
