e69c7b259ff3ef9dc2b076d8944e78a871eb86e3
leonel ka <waboleonel@gmail.com>
2024-10-26 23:49:38 +0100

    Payment service - code commented

--- services/payment-service/src/main/kotlin/com/workbeattalent/paymentservice/PaymentServiceApplication.kt
+++ services/payment-service/src/main/kotlin/com/workbeattalent/paymentservice/PaymentServiceApplication.kt
@@ -2,12 +2,14 @@ package com.workbeattalent.paymentservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+//import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
 @EnableJpaAuditing
+//@EnableFeignClients
 class PaymentServiceApplication
 
 fun main(args: Array<String>) {
-	runApplication<PaymentServiceApplication>(*args)
+    runApplication<PaymentServiceApplication>(*args)
 }
