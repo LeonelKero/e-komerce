ccaab053c14c9a77ebd46fc484b27f19093c5cbd
leonel ka <waboleonel@gmail.com>
2024-11-10 08:52:06 +0100

    Notification-rmk service - fix import

---
+++ services/notification-rmk/src/main/java/com/workbeattalent/notification/NotificationRmkApplication.java
@@ -0,0 +1,13 @@
+package com.workbeattalent.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationRmkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationRmkApplication.class, args);
+	}
+
+}
